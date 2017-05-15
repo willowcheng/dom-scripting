002a9354498a1cbf9ddeb67f3bc117e43d335186
willowcheng <e.wingus@gmail.com>
2017-05-15 14:52:56 -0400

    RSVP checkbox

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const form = document.getElementById('registrar')
 const input = form.querySelector('input')
+const ul = document.getElementById('invitedList')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -8,5 +9,23 @@ form.addEventListener('submit', (e) => {
   const ul = document.getElementById('invitedList')
   const li = document.createElement('li')
   li.textContent = text
+  const label = document.createElement('label')
+  label.textContent = 'Confirmed'
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  label.appendChild(checkbox)
+  li.appendChild(label)
   ul.appendChild(li)
+})
+
+ul.addEventListener('change', (e) => {
+  const checkbox = e.target
+  const checked = checkbox.checked
+  const listItem = checkbox.parentNode.parentNode
+
+  if (checked) {
+    listItem.className = 'responded'
+  } else {
+    listItem.className = ''
+  }
 })
