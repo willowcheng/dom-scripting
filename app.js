d144e9bb06e35f7dfe3122d78babc64f4653be55
willowcheng <e.wingus@gmail.com>
2017-05-18 17:56:05 -0400

    Remove names

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const form = document.getElementById('registrar')
 const input = form.querySelector('input')
 const ul = document.getElementById('invitedList')
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const text = input.value
-  input.value = ''
-  const ul = document.getElementById('invitedList')
+function createLI(text) {
   const li = document.createElement('li')
   li.textContent = text
   const label = document.createElement('label')
@@ -15,6 +11,17 @@ form.addEventListener('submit', (e) => {
   checkbox.type = 'checkbox'
   label.appendChild(checkbox)
   li.appendChild(label)
+  const button = document.createElement('button')
+  button.textContent = 'remove'
+  li.appendChild(button)
+  return li
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const text = input.value
+  input.value = ''
+  const li = createLI(text)
   ul.appendChild(li)
 })
 
@@ -27,5 +34,13 @@ ul.addEventListener('change', (e) => {
     listItem.className = 'responded'
   } else {
     listItem.className = ''
+  }
+})
+
+ul.addEventListener('click', (e) => {
+  if(e.target.tagName === 'BUTTON') {
+    const li = e.target.parentNode
+    const ul = li.parentNode
+    ul.removeChild(li)
   }
 })
