726ee434a073ee50a3e706e1905710681f65d0db
willowcheng <e.wingus@gmail.com>
2017-06-05 22:27:13 -0400

    Move to an editing state

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const ul = document.getElementById('invitedList');
 
 function createLI(text) {
   const li = document.createElement('li');
-  li.textContent = text;
+  const span = document.createElement('span');
+  span.textContent = text;
+  li.appendChild(span);
   const label = document.createElement('label');
   label.textContent = 'Confirmed';
   const checkbox = document.createElement('input');
@@ -50,7 +52,13 @@ ul.addEventListener('click', (e) => {
     if (button.textContent === 'remove') {
       ul.removeChild(li);
     } else if (button.textContent === 'edit') {
-      console.log('edit');
+      const span = li.firstElementChild;
+      const input = document.createElement('input');
+      input.type = 'text';
+      input.value = span.textContent;
+      li.insertBefore(input, span);
+      li.removeChild(span);
+      button.textContent = 'save';
     }
   }
 });  
