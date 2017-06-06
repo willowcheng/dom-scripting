38ead8a083c1cd740e1b96458fda5c267abe73c1
willowcheng <e.wingus@gmail.com>
2017-06-05 22:19:39 -0400

    States of the application

--- app.js
+++ app.js
@@ -1,46 +1,66 @@
-const form = document.getElementById('registrar')
-const input = form.querySelector('input')
-const ul = document.getElementById('invitedList')
+const form = document.getElementById('registrar');
+const input = form.querySelector('input');
+const ul = document.getElementById('invitedList');
 
 function createLI(text) {
-  const li = document.createElement('li')
-  li.textContent = text
-  const label = document.createElement('label')
-  label.textContent = 'Confirmed'
-  const checkbox = document.createElement('input')
-  checkbox.type = 'checkbox'
-  label.appendChild(checkbox)
-  li.appendChild(label)
-  const button = document.createElement('button')
-  button.textContent = 'remove'
-  li.appendChild(button)
-  return li
+  const li = document.createElement('li');
+  li.textContent = text;
+  const label = document.createElement('label');
+  label.textContent = 'Confirmed';
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  label.appendChild(checkbox);
+  li.appendChild(label);
+
+  const editButton= document.createElement('button');
+  editButton.textContent = 'edit';
+  li.appendChild(editButton);
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'remove';
+  li.appendChild(removeButton);
+  return li;
 }
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const text = input.value
-  input.value = ''
-  const li = createLI(text)
-  ul.appendChild(li)
-})
-
+  e.preventDefault();
+  const text = input.value;
+  input.value = '';
+  const li = createLI(text);
+  ul.appendChild(li);
+});
+  
 ul.addEventListener('change', (e) => {
-  const checkbox = e.target
-  const checked = checkbox.checked
-  const listItem = checkbox.parentNode.parentNode
-
+  const checkbox = event.target;
+  const checked = checkbox.checked;
+  const listItem = checkbox.parentNode.parentNode;
+  
   if (checked) {
-    listItem.className = 'responded'
+    listItem.className = 'responded';
   } else {
-    listItem.className = ''
+    listItem.className = '';
   }
-})
-
+});
+  
 ul.addEventListener('click', (e) => {
-  if(e.target.tagName === 'BUTTON') {
-    const li = e.target.parentNode
-    const ul = li.parentNode
-    ul.removeChild(li)
+  if (e.target.tagName === 'BUTTON') {
+    const button = e.target;
+    const li = button.parentNode;
+    const ul = li.parentNode;
+    if (button.textContent === 'remove') {
+      ul.removeChild(li);
+    } else if (button.textContent === 'edit') {
+      console.log('edit');
+    }
   }
-})
+});  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
